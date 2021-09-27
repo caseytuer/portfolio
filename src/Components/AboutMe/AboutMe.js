289bdb56c8d17d5a85b9a3f09cf945af725dcf25
Casey Tuer <caseytuer@gmail.com>
2021-09-27 08:42:23 -0700

    fixed spelling error on about me

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -3,7 +3,7 @@ import casey from '../../assets/casey.png';
 import kona from '../../assets/kona.png';
 import './AboutMe.css';
 
-const aboutMe1 = "I have always been fascinated with technology and aspired to one day pursue a career in software development. After years of learning to code on my own while working full time in the service industry, I made the decision to attended App Academy's 1500 hr full stack software engineering program. Here I was formally trained in software development and introduced to the most in-demand programming tools to date." 
+const aboutMe1 = "I have always been fascinated with technology and aspired to one day pursue a career in software development. After years of learning to code on my own while working full time in the service industry, I made the decision to attend App Academy's 1500 hr full stack software engineering program. Here I was formally trained in software development and introduced to the most in-demand programming tools to date." 
 const aboutMe2 = "Since then I've been sharpening my skills in various frontend and backend frameworks and libraries by building full stack applications from the ground up and collaborating with my peers." 
 const aboutMe3 = "When I'm not programming I am volunteering with Urban Surf 4 Kids, fostering animals, training my rescue pitty Kona, or hanging out at the beach in beautiful San Diego."
 
